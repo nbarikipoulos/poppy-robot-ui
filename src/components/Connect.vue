67db8c6f5d4c7d8e94b6c0c34df5bc3ab4e0895a
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-01-20 17:14:24 +0100

    Connection component reshaped
    Introduce both http and snap ports
    
    Add in-progress when trying to connect
    
    display notification when connection failed

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -1,10 +1,17 @@
 <template lang="pug">
-  section
-    div(class="box")
-      b-field
-        b-input(type="text" v-model="store.hostname")
-        p(class="control")
-          b-button(type="is-primary" @click="connect") Connect
+  section(class="hero is-medium")
+    div(class="hero-body")
+      div(class="columns is-centered is-vcentered")
+        div(class="column is-one-quarter")
+          div(class="box")
+            b-field(label="Hostname")
+              b-input(type="text" v-model="store.hostname")
+            b-field(label="Http port")
+              b-input(type="text" v-model="store.httpPort")
+            b-field(label="Snap Port")
+              b-input(type="text" v-model="store.snapPort")
+            b-button(type="is-primary" @click="connect" expanded) Connect
+    b-loading(:is-full-page="false" :active.sync="isConnecting" :can-cancel="true")
 </template>
 
 <script>
@@ -15,14 +22,27 @@ export default {
   name: 'Connect',
   data () {
     return {
-      store
+      store,
+      isConnecting: false
     }
   },
   props: {
 
   },
   methods: {
-    async connect () { await store.init() }
+    async connect () {
+      this.isConnecting = true
+      await store.init()
+      this.isConnecting = false
+      if (!store.isConnected()) {
+        this.$buefy.notification.open({
+          duration: 5000,
+          position: 'is-bottom',
+          message: 'Unable to connect.',
+          type: 'is-danger'
+        })
+      }
+    }
   }
 }
 </script>
