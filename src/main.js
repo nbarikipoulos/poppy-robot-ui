2fbe75cb10c9b147fe7b0ca27e21d5876ba5bb0b
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-02-02 00:31:28 +0100

    Introducing chart package

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 
 import Vue from 'vue'
 import Buefy from 'buefy'
+import VueApexCharts from 'vue-apexcharts'
 
 import router from '@/router/router'
 
 import App from '@/App.vue'
-
 Vue.use(Buefy)
+Vue.component('apexchart', VueApexCharts)
+
 Vue.config.productionTip = false
 
 new Vue({
