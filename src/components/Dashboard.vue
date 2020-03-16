d319cb22409b6e073d44636ce6697be0d571f348
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-03-16 15:33:03 +0100

    Append register table

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,31 +1,14 @@
 <template lang="pug">
   section
     div(class="box columns is-multiline")
-      div(
-        v-for="desc in getMotorDescriptors()"
-        class="column is-6"
-      )
-        MotorChart(
-          class="box has-background-primary"
-          :name="desc.name"
-          :data="store.mdata[desc.name].data"
-          :height="100"
-        )
+      Registers(class="column is-6")
 </template>
 
 <script>
-
-import store from '@/lib/store'
+import Registers from '@/components/Registers'
 
 export default {
   name: 'Dashboard',
-  data () {
-    return {
-      store
-    }
-  },
-  methods: {
-    getMotorDescriptors () { return this.store.getRobotDescriptor().motors }
-  }
+  components: { Registers }
 }
 </script>
