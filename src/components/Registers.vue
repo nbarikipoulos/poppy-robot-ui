8c193f12e88fdf66d181424f55cb5252d723565e
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-03-16 15:32:11 +0100

    Component to display registers

---
+++ src/components/Registers.vue
@@ -0,0 +1,69 @@
+<template lang="pug">
+  div(class="box table-container")
+    table(
+      class="table is-fullwidth has-text-primary has-text-weight-bold is-size-5"
+      :class="clazz"
+    )
+      thead
+        th id
+        th
+          b-icon(pack="fas" icon="crosshairs")
+        th
+          b-icon(pack="fas" icon="gamepad")
+        th
+          b-icon(pack="fas" icon="tachometer-alt")
+        th
+          b-icon(pack="far" icon="lightbulb")
+        th
+          b-icon(pack="fas" icon="thermometer-half")
+      tr(
+        v-for="motor in getMotorIds()"
+        :class="clazz"
+      )
+        td
+          span {{ motor }}
+        td
+          span {{ Math.round(mdata[motor].present_position) }}
+        td
+          ExtIcon(:value="mdata[motor].compliant" :state="icons.compliant")
+        td
+          span {{ mdata[motor].moving_speed }}
+        td
+          ExtIcon(:value="mdata[motor].led" :state="icons.led")
+        td
+          span {{ mdata[motor].present_temperature }}
+</template>
+
+<script>
+'use strict'
+
+import store from '@/lib/store'
+
+const icons = {
+  compliant: [
+    { value: true, icon: 'bed', pack: 'fas' },
+    { value: false, icon: 'bolt', pack: 'fas' }
+  ],
+  led: (value, old) => ({
+    pack: value === 'off' ? 'fas' : 'fa',
+    icon: value === 'off' ? 'minus' : 'sun',
+    type: value === 'off' ? 'is-primary' : `is-${value}`
+  })
+}
+
+export default {
+  name: 'Registers',
+  data () {
+    return {
+      icons,
+      clazz: 'is-narrow has-text-centered',
+      mdata: store.mdata
+    }
+  },
+  props: {
+  },
+  methods: {
+    getMotorIds () { return store.getPoppy().getAllMotorIds() }
+  }
+}
+</script>
