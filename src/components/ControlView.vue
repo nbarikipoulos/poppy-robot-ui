787153643bdcbda9495011d358bfd1f182c4d579
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-04-03 23:07:05 +0200

    Rename root components

---
+++ src/components/ControlView.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+  section
+    div(class="box")
+      div(class="columns is-mobile")
+        div(class="column is-narrow")
+          b-field
+            b-switch(
+              v-model="controlMotors"
+              type="is-primary-bis"
+              :rounded="false"
+            ) Control motors
+        div(class="column")
+          b-field(
+            custom-class="has-text-primary-bis"
+            label="Speed"
+            label-position="on-border"
+          )
+            b-input(v-model="speed" placeholder="Set all motors speed")
+            p(class="control")
+              b-button(type="is-primary-bis" @click="setSpeed") Set
+    div(class="columns is-multiline")
+      div(
+        v-for="motorId in motorIds"
+        class="column is-half-tablet is-one-quarter-desktop"
+      )
+        MotorControl(
+          :motorId="motorId"
+          :compliant="!controlMotors"
+        )
+</template>
+
+<script>
+import MotorControl from '@/components/MotorControl.vue'
+
+import store from '@/lib/store'
+
+export default {
+  name: 'ControlView',
+  components: {
+    MotorControl
+  },
+  data: _ => ({
+    controlMotors: false,
+    speed: null
+  }),
+  computed: {
+    motorIds: function () { return store.getAllMotorIds() }
+  },
+  watch: {
+    controlMotors: async function (value) {
+      await store.pConnector.execute(
+        'compliant',
+        ['all'],
+        !value
+      )
+    }
+  },
+  methods: {
+    async setSpeed () {
+      if (this.speed) {
+        await store.pConnector.execute(
+          'speed',
+          ['all'],
+          this.speed
+        )
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/DashboardView.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  section
+    div(class="tile is-ancestor")
+      div(class="tile is-parent is-8")
+        div(class="tile is-child box")
+          Registers
+      div(class="tile is-parent is-4")
+        div(class="tile is-child box")
+          PolarPositionsChart
+</template>
+
+<script>
+import Registers from '@/components/Registers'
+import PolarPositionsChart from '@/components/PolarPositionsChart'
+
+export default {
+  name: 'DashboardView',
+  components: { Registers, PolarPositionsChart }
+}
+</script>
